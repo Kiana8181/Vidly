5b12ea6646abe5cebcddf8e4308fcf375dd0b1c6
Kiana8181 <kianahashemi8@gmail.com>
2022-08-28 12:26:53 +0430

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { getMovies } from "./services/fakeMovieService";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Movie extends Component {
+  state = { movies: getMovies() };
+  handelDelete = (movie) => {
+    const newList = this.state.movies.filter((m) => m !== movie);
+    this.setState({ movies: newList });
+  };
+  render() {
+    return (
+      <main className="container mt-5">
+        {this.state.movies.length === 0 ? (
+          <h3>There are no movies in the database.</h3>
+        ) : (
+          <h3>Showing {this.state.movies.length} movies in the database.</h3>
+        )}
+        <table className="table table-striped mt-3">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handelDelete(movie)}
+                    type="button"
+                    className="btn btn-danger"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
+    );
+  }
 }
 
-export default App;
+export default Movie;
