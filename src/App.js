3b41d62cde0ea05371341b4c5aa1e3feeca1ae27
Kiana8181 <kianahashemi8@gmail.com>
2022-09-29 02:42:11 +0330

    logout finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import jwtDecode from "jwt-decode";
 import Movie from "./components/movie";
 import Rentals from "./components/rentals";
 import Customers from "./components/customers";
@@ -9,18 +10,28 @@ import NotFound from "./components/notFound";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
+import Logout from "./components/logout";
 import "react-toastify/dist/ReactToastify.css";
 
 class App extends Component {
+  state = {};
+  componentDidMount() {
+    try {
+      const jwt = localStorage.getItem("token");
+      const user = jwtDecode(jwt);
+      this.setState({ user });
+    } catch (ex) {}
+  }
   render() {
     return (
       <React.Fragment>
         <ToastContainer></ToastContainer>
-        <NavBar />
+        <NavBar user={this.state.user} />
         <main className="container m-5">
           <Switch>
             <Route path="/register" component={RegisterForm} />
             <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movie} />
             <Route path="/rentals" component={Rentals} />
